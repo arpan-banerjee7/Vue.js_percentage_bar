8d62ae32267faeb9673c1a216acedac3ae38017b
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-07-16 13:03:48 +0530

    final project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app">
+		<Post title="Post Title" />
+		<Post title="Post Title 2" />
+	</div>
+</template>
+
+<script>
+	import Post from './components/Post';
+
+	export default {
+		name: 'app',
+		components: {
+			Post
+		}
+	}
+</script>
+
+<style lang="sass">
+	body
+		margin: 0
+		padding: 0
+		background:
+			color: #F3F3F3
+		#app
+			padding: 25px
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,144 @@
+<template>
+	<div>
+		<div class="percentage-bar" id="pb">
+			<div class="fill"></div>
+			<p>{{ percentage }}%</p>
+		</div>
+		<article v-on:scroll="onScroll">
+			<h1>
+				{{ title }}
+			</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni esse eaque, temporibus quas cupiditate impedit. Dolor, a? Assumenda officiis rem ab veritatis error illo perferendis dolorem, quod officia impedit nihil.
+				<br><br>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit odio fuga, aut architecto, vero, doloribus iure soluta omnis doloremque quaerat aperiam facere ab id ullam magni saepe voluptate suscipit minus.
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit odio fuga, aut architecto, vero, doloribus iure soluta omnis doloremque quaerat aperiam facere ab id ullam magni saepe voluptate suscipit minus.
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit odio fuga, aut architecto, vero, doloribus iure soluta omnis doloremque quaerat aperiam facere ab id ullam magni saepe voluptate suscipit minus.
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit odio fuga, aut architecto, vero, doloribus iure soluta omnis doloremque quaerat aperiam facere ab id ullam magni saepe voluptate suscipit minus.
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit odio fuga, aut architecto, vero, doloribus iure soluta omnis doloremque quaerat aperiam facere ab id ullam magni saepe voluptate suscipit minus.
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit odio fuga, aut architecto, vero, doloribus iure soluta omnis doloremque quaerat aperiam facere ab id ullam magni saepe voluptate suscipit minus.
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit odio fuga, aut architecto, vero, doloribus iure soluta omnis doloremque quaerat aperiam facere ab id ullam magni saepe voluptate suscipit minus.
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit odio fuga, aut architecto, vero, doloribus iure soluta omnis doloremque quaerat aperiam facere ab id ullam magni saepe voluptate suscipit minus.
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit odio fuga, aut architecto, vero, doloribus iure soluta omnis doloremque quaerat aperiam facere ab id ullam magni saepe voluptate suscipit minus.
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit odio fuga, aut architecto, vero, doloribus iure soluta omnis doloremque quaerat aperiam facere ab id ullam magni saepe voluptate suscipit minus.
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit odio fuga, aut architecto, vero, doloribus iure soluta omnis doloremque quaerat aperiam facere ab id ullam magni saepe voluptate suscipit minus.
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+				<br><br>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit odio fuga, aut architecto, vero, doloribus iure soluta omnis doloremque quaerat aperiam facere ab id ullam magni saepe voluptate suscipit minus.
+				<br><br>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, iusto optio possimus saepe est dignissimos eligendi magni esse quo culpa perspiciatis enim maiores, quisquam aperiam porro molestias eos, et consequatur?
+			</p>
+		</article>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Post",
+		props: ["title"],
+		data: function () {
+			return {
+				percentage: 0
+			}
+		},
+		methods: {
+			onScroll: function (evt) {
+				let post = evt.target;
+				let st = post.scrollTop;
+				let sh = post.scrollHeight;
+				let ch = post.clientHeight;
+				
+				let percent = Math.floor((st / (sh - ch)) * 100);
+
+				this.percentage = percent;
+
+				const pb = post.parentNode.firstChild;
+				for (let i = 0; i < pb.children.length; i++) {
+					let fillbar = pb.children[i];
+					if (fillbar.classList.contains("fill")) {
+						fillbar.style.width = this.percentage + "%";
+					}
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="sass" scoped>
+	article
+		width: 100%
+		max-width: 500px
+		height: 100%
+		max-height: 300px
+		overflow-y: scroll
+		margin: 0 auto
+		padding: 15px
+		background:
+			color: #FFFFFF
+		position: relative
+	.percentage-bar
+		position: relative
+		width: 100%
+		max-width: 530px
+		height: 25px
+		margin: 0 auto
+		background:
+			color: #171717
+		.fill
+			position: absolute
+			width: 0
+			top: 0
+			left: 0
+			bottom: 0
+			background:
+				color: #FFCE00
+			z-index: 0
+		p
+			position: absolute
+			width: 100%
+			font:
+				size: 18px
+			text-align: center
+			line-height: 25px
+			color: #F3F3F3
+			margin: 0
+			z-index: 1
+</style>
+
